b1ea4995bdc004e44c945d9acf7df2a1a554da99
QKM-0203 <com_qkm@xiyoulinux.org>
2021-07-17 22:47:54 +0800

    符号

--- src/main/java/Operator/Operator.java
+++ src/main/java/Operator/Operator.java
@@ -1,10 +1,12 @@
 package Operator;
 
 public class Operator {
-    public static final String CREATE_TAG = "create tag " ;
-    public static final String DROP_TAG = "drop tag " ;
-    public static final String ALTER_TAG = "alter tag " ;
-    public static final String SHOW_TAGs = "show tags" ;
+    public static final String TAG = "tag";
+    public static final String CREATE = "create " ;
+    public static final String DROP = "drop " ;
+    public static final String ALTER = "alter " ;
+    public static final String SHOW = "show " ;
+    public static final String DESCRIBE = "describe " ;
     public static final String IF_NOT_EXIST = "if not exists " ;
     public static final String IF_EXIST = "if exist " ;
     public static final String LEFT_BRACKETS = "(" ;
