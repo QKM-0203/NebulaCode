8fb391358914cb0527229956e438629735c8d251
QKM-0203 <com_qkm@xiyoulinux.org>
2021-07-20 22:36:52 +0800

    添加Space、Node对象

--- src/main/java/entity/Node.java
+++ src/main/java/entity/Node.java
@@ -1,60 +1,41 @@
 package entity;
-import ogm.Tag;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 public class Node {
 
-    //图空间中全局唯一vid
-    private final Object vid;
+    //vertexList
+    private List<Object> vid;
 
-    //标签集合,仅检测标签的名称，不会检测具体属性。
-    private Set<Tag> tagSet =  new HashSet<>();
+     //propMap
+    private HashMap<String, HashMap<String,Object>> propMap;
 
-    public Node(Object vid,Set<Tag> tagSet){
-        this.tagSet = tagSet;
+    public Node(List<Object> vid, HashMap<String, HashMap<String, Object>> propMap) {
+        this.vid = vid;
+        this.propMap = propMap;
+    }
+
+    public Node(Object vid, HashMap<String, HashMap<String, Object>> propMap) {
+        this.vid = new ArrayList<>();
+        this.vid.add(vid);
+        this.propMap = propMap;
+    }
+
+    public Node() {
+    }
+
+    public void setVid(List<Object> vid) {
         this.vid = vid;
     }
 
 
-    public Node(Object vid,Tag tagSet){
-        this.tagSet.add(tagSet);
-        this.vid = vid;
+    public void setVid(Object vid) {
+        this.vid.add(vid);
     }
 
 
-    //打印出当前Node的信息
-    public String getNode(){
-        //利用vid
-        return "";
+    public void setPropMap(HashMap<String, HashMap<String, Object>> propMap) {
+        this.propMap = propMap;
     }
-
-    //判断是否含有某个标签
-    public boolean isHasTag(String tagName){
-        //...
-        return true;
-    }
-
-    //增加新的标签
-    public boolean addTag(String tagName,Map<String, Object> tag){
-        //...重新插入点带上新的标签
-        return true;
-    }
-
-    //删除Node所有的标签,则该Node也就不存在了
-    public boolean clearAllTags(){
-        return true;
-    }
-
-    //更新Node节点某个标签的属性值
-    public boolean updateTag(String tagName,Map<String, Object> tag){
-        return true;
-    }
-
-
-    //....等等
-
 }
