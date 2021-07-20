736552689006208785cf3ed73b2434006b4a94b6
QKM-0203 <com_qkm@xiyoulinux.org>
2021-07-20 15:42:12 +0800

    使用枚举

--- src/main/java/common/Operate.java
+++ src/main/java/common/Operate.java
@@ -4,5 +4,4 @@ public  interface Operate<T>  {
     void create(T Entity) throws NoSuchFieldException, IllegalAccessException;
     void drop(T Entity) throws NoSuchFieldException, IllegalAccessException;
     void update(T Entity);
-
 }
