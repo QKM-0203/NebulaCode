fc856f6b3cd5b884f35d62b0f2eb275af46a596b
QKM-0203 <com_qkm@xiyoulinux.org>
2021-07-20 22:40:31 +0800

    测试Space、Node对象

--- src/test/java/Text.java
+++ src/test/java/Text.java
@@ -1,26 +1,22 @@
-import com.facebook.thrift.TException;
+import Operator.Type;
 import com.vesoft.nebula.client.graph.NebulaPoolConfig;
-import com.vesoft.nebula.client.graph.data.HostAddress;
 import com.vesoft.nebula.client.graph.data.ResultSet;
 import com.vesoft.nebula.client.graph.data.ValueWrapper;
 import com.vesoft.nebula.client.graph.exception.IOErrorException;
 import com.vesoft.nebula.client.graph.net.Session;
-import com.vesoft.nebula.client.meta.MetaManager;
-import entity.ConnectionProfile;
-import entity.Graph;
-import entity.GraphService;
-import ogm.Tag;
+import entity.*;
 import operation.TagOperation;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class TextGraphService {
-    private static final Logger log = LoggerFactory.getLogger(TextGraphService.class);
+public class Text {
+    private static final Logger log = LoggerFactory.getLogger(Text.class);
     private static void printResult(ResultSet resultSet) throws UnsupportedEncodingException {
         List<String> colNames = resultSet.keys();
         // System.out.println(colNames.toString());
@@ -122,20 +118,58 @@ public class TextGraphService {
         tagOperation.drop(tag);
     }
 
+
     @Test
-    public void test_get_tags() throws NoSuchFieldException, IllegalAccessException {
-        TagOperation tagOperation = new TagOperation(new ConnectionProfile());
-        ResultSet tags = tagOperation.getTags();
-        System.out.println(tags);
+    public void test_add_TagProp()  {
+        Property property = new Property("name", Type.STRING,false,null);
+        Property property1 = new Property("age", Type.INT8,false,null);
+        ArrayList<Property> properties = new ArrayList<>();
+        properties.add(property);
+        properties.add(property1);
+        Tag tag = new Tag("QKM2",properties);
     }
 
     @Test
-    public void test_get_tagStructure() throws NoSuchFieldException, IllegalAccessException {
-        TagOperation tagOperation = new TagOperation(new ConnectionProfile());
-        Tag qkm2 = new Tag("QKM2");
-        ResultSet tags = tagOperation.getTagStructure(qkm2);
-        System.out.println(tags);
+    public void test_add_space() {
+        Space space = new Space("QKM",10,1,Type.FIXED_STRING.setLength(30));
+        Space space1 = new Space("QKM",10,1,Type.INT64);
     }
+
+
+    @Test
+    public void test_add_vertex(){
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        HashMap<String, HashMap<String, Object>> propMap = new HashMap<>();
+        HashMap<String, Object> propValue = new HashMap<>();
+        propValue.put("name","asd");
+        propValue.put("age",19);
+        propMap.put("QKM2",propValue);
+        //插入一个点，该点含有一个标签
+        Node node1 = new Node(1, propMap);
+        Node node2 = new Node("1", propMap);
+
+
+        //插入多个点，每个点都有一个标签
+        Node node3 = new Node(integers,propMap);
+
+        HashMap<String, Object> propValue1 = new HashMap<>();
+        propValue1.put("a",12);
+        propValue1.put("b",15);
+        propMap.put("QKm3",propValue1);
+
+
+        //插入一个点，每个点都有多个标签
+        Node node4 = new Node(1, propMap);
+
+        //插入多个点，每个点都有多个分支
+        Node node5 = new Node(integers, propMap);
+
+
+    }
+
+
 
 
 }
