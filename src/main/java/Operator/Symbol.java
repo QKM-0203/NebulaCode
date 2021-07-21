e2b75fa65f8215ee6d1a833c1cf735d96b12a04a
QKM-0203 <com_qkm@xiyoulinux.org>
2021-07-21 11:39:59 +0800

    delete all

--- src/main/java/Operator/Symbol.java
+++
@@ -1,9 +0,0 @@
-package Operator;
-
-public class Symbol {
-
-    public static final String LEFT_BRACKETS = "(";
-    public static final String RIGHT_BRACKETS = ")";
-    public static final String SPACE = " ";
-    public static final String COMMA = ",";
-}
--- src/main/java/common/Operate.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-public  interface Operate<T>  {
-    void create(T Entity) throws NoSuchFieldException, IllegalAccessException;
-    void drop(T Entity) throws NoSuchFieldException, IllegalAccessException;
-    void update(T Entity);
-}
