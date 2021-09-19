a4a33f8cf43367762845f7396b4009a85c58bb71
QKM-0203 <com_qkm@xiyoulinux.org>
2021-09-19 15:53:39 +0800

    modify lexer to KeyWord and delete space

---
+++ src/main/java/com/vesoft/nebula/orm/query/util/KeyWord.java
@@ -0,0 +1,74 @@
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+package com.vesoft.nebula.orm.query.util;
+
+/**
+ * keywords.
+ *
+ * @author Qi Kai Meng
+ */
+public class KeyWord {
+    public static final String MATCH = "MATCH";
+    public static final String FROM = "FROM";
+    public static final String RETURN = "RETURN";
+    public static final String FETCH_PROP_ON = "FETCH PROP ON";
+    public static final String YIELD = "YIELD";
+    public static final String GO = "GO";
+    public static final String GET_SUBGRAPH = "GET SUBGRAPH";
+    public static final String FIND = "FIND";
+    public static final String PATH = " PATH";
+    public static final String LOOKUP = "LOOKUP ON";
+    public static final String ORDER_BY = "ORDER BY";
+    public static final String GROUP_BY = "GROUP BY";
+    public static final String SKIP = "SKIP";
+    public static final String STEPS = "STEPS";
+    public static final String PIPE = "|";
+    public static final String TO = "TO";
+    public static final String TTL_DURATION = "TTL_DURATION";
+    public static final String TTL_COL = "TTL_COL";
+    public static final String NULL = "NULL";
+    public static final String NOT_NULL = "NOT NULL";
+    public static final String DEFAULT = " DEFAULT";
+    public static final String AND = "AND";
+    public static final String WHERE = "WHERE";
+    public static final String AS = "AS";
+    public static final String FIXED_STRING = "FIXED_STRING";
+    public static final String DATETIME = "DATETIME";
+    public static final String TIME = "TIME";
+    public static final String DATE = "DATE";
+    public static final String ALL = "*";
+    public static final String OVER = "OVER";
+    public static final String UPTO = "UPTO";
+    public static final String LIMIT = "LIMIT";
+    public static final String DELETE = "DELETE";
+    public static final String VERTEX = "VERTEX";
+    public static final String EDGE = "EDGE";
+    public static final String USE = "USE";
+    public static final String INSERT = "INSERT";
+    public static final String VALUES = "VALUES";
+    public static final String DESC = "DESC";
+    public static final String TAG = "TAG";
+    public static final String CREATE = "CREATE";
+    public static final String INDEX = "INDEX";
+    public static final String IF_NOT_EXISTS = "IF NOT EXISTS";
+    public static final String IF_EXISTS = "IF EXISTS";
+    public static final String ON = "ON";
+    public static final String SHOW = "SHOW";
+    public static final String TAGS = "TAGS";
+    public static final String EDGES = "EDGES";
+    public static final String DROP = "DROP";
+    public static final String STATS = "STATS";
+    public static final String SPACE = "SPACE";
+    public static final String PARTITION_NUM = "PARTITION_NUM";
+    public static final String REPLICA_FACTOR = "REPLICA_FACTOR";
+    public static final String VID_TYPE = "VID_TYPE";
+    public static final String USERS = "USERS";
+    public static final String HOSTS = "HOSTS";
+    public static final String SNAPSHOTS = "SNAPSHOTS";
+    public static final String PARTS = "PARTS";
+    public static final String $_PATH = "$-.PATH";
+}
